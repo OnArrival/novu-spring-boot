1c3b6646e59113c4f11bad60a9855ddf962aa51e
Pawan Shetty <pawanshetty@outlook.com>
2022-11-01 16:10:16 +0530

    Initial commit

---
+++ src/main/kotlin/co/novu/springbootnovuclient/SpringBootNovuClientApplication.kt
@@ -0,0 +1,11 @@
+package co.novu.springbootnovuclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootNovuClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootNovuClientApplication>(*args)
+}
---
+++ src/test/kotlin/co/novu/springbootnovuclient/SpringBootNovuClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.novu.springbootnovuclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootNovuClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
