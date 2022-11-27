2aec9f9ba589739cd5dfcd0908b38f15d9d2a526
suvidh Adhikari k <adhikarisuvidh@gmail.com>
2022-11-27 20:18:01 +0530

    fixing auth config

--- src/main/kotlin/co/novu/springbootnovuclient/configaration/NovuClientRequestInterceptor.kt
+++ src/main/kotlin/co/novu/springbootnovuclient/configaration/NovuClientRequestInterceptor.kt
@@ -6,16 +6,14 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class NovuClientRequestInterceptor : RequestInterceptor {
+open class NovuClientRequestInterceptor : RequestInterceptor {
     private val AUTHORIZATION_HEADER = "Authorization"
-    private val TOKEN_TYPE = " ApiKey"
+    private val TOKEN_TYPE = "ApiKey"
 
-    @Value("\${novu.app.key}")
-    val novuApiKey: String = ""
 
     //need to interface for getting auth
 
     override fun apply(requestTemplate: RequestTemplate) {
-        requestTemplate.header(AUTHORIZATION_HEADER, String.format("%s %s", TOKEN_TYPE, novuApiKey))
+        requestTemplate.header(AUTHORIZATION_HEADER, String.format("%s %s", TOKEN_TYPE, "{{key form config bean}}"))
     }
 }
