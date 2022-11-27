7fcf31d2f0c9a76a88a9e7ce9870d9d235c089cb
suvidh Adhikari k <adhikarisuvidh@gmail.com>
2022-11-27 20:16:03 +0530

    spring stable version java compatible version setting feign compatible with spring cloud version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.0-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.20"
-	kotlin("plugin.spring") version "1.7.20"
+	id("org.springframework.boot") version "2.5.5"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.5.31"
+	kotlin("plugin.spring") version "1.5.31"
 }
 
 group = "co.novu"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -21,20 +21,19 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation ("org.springframework.cloud:spring-cloud-starter-openfeign")
+	implementation ("org.springframework.cloud:spring-cloud-starter-openfeign:3.0.7")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 dependencyManagement {
 	imports {
-		mavenBom ("org.springframework.cloud:spring-cloud-dependencies:Hoxton.SR5")
+		mavenBom ("org.springframework.cloud:spring-cloud-dependencies:2020.0.6")
 	}
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
 	}
 }
 
